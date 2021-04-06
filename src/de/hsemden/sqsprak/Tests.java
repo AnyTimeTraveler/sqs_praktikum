ca3c462b057fa4650b857c0e472bc589042051ce
Simon Struck <simon@simonscode.org>
2021-04-06 08:32:03 +0200

    Add tests

--- src/de/hsemden/sqsprak/Tests.java
+++ src/de/hsemden/sqsprak/Tests.java
@@ -4,12 +4,24 @@ import org.junit.Test;
 
 import static de.hsemden.sqsprak.TriangleChecker.checkTriangle;
 import static de.hsemden.sqsprak.TriangleType.EQUILATERAL_TRIANGLE;
+import static de.hsemden.sqsprak.TriangleType.NO_TRIANGLE;
 import static org.junit.Assert.assertEquals;
 
 public class Tests {
 
     @Test
-    public void equalTest(){
-        assertEquals(EQUILATERAL_TRIANGLE, checkTriangle(6,6,6));
+    public void equilateralTest() {
+        for (int i = 1; i < 10; i++) {
+            assertEquals(EQUILATERAL_TRIANGLE, checkTriangle(i, i, i));
+        }
+    }
+
+    @Test
+    public void noTriangleTest() {
+        for (int i = 1; i < 10; i++) {
+            assertEquals(NO_TRIANGLE, checkTriangle(i * 3, i, i));
+            assertEquals(NO_TRIANGLE, checkTriangle(i, i * 3, i));
+            assertEquals(NO_TRIANGLE, checkTriangle(i, i, i * 3));
+        }
     }
 }
--- src/de/hsemden/sqsprak/TriangleChecker.java
+++ src/de/hsemden/sqsprak/TriangleChecker.java
@@ -2,7 +2,9 @@ package de.hsemden.sqsprak;
 
 public class TriangleChecker {
     public static TriangleType checkTriangle(int a, int b, int c) {
+        if (a == b && b == c) {
+            return TriangleType.EQUILATERAL_TRIANGLE;
+        }
         return TriangleType.NO_TRIANGLE;
     }
 }
-
