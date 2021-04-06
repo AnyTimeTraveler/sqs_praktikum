2a046aa8e50cffc0261b34656b8a82987e3fba82
Philip <norikitamo@web.de>
2021-04-06 08:57:10 +0200

    added other trianangles

--- src/de/hsemden/sqsprak/TriangleChecker.java
+++ src/de/hsemden/sqsprak/TriangleChecker.java
@@ -1,10 +1,15 @@
 package de.hsemden.sqsprak;
 
 public class TriangleChecker {
-    public static TriangleType checkTriangle(int a, int b, int c) {
-        if (a == b && b == c) {
-            return TriangleType.EQUILATERAL_TRIANGLE;
-        }
-        return TriangleType.NO_TRIANGLE;
+
+  public static TriangleType checkTriangle(int a, int b, int c) {
+    if (a == b && b == c) {
+      return TriangleType.EQUILATERAL_TRIANGLE; // gleichseitig
+    } else if (a != b && a != c && b != c) {
+      return TriangleType.SCALENE_TRIANGLE; // ungleichseitig
+    } else if (a == b && a + b > c) {
+      return TriangleType.ISOSCELES_TRIANGLE; // gleichschenklig
     }
+    return TriangleType.NO_TRIANGLE;
+  }
 }
