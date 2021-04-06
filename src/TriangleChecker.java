252fd219be03f35a5466af7ca43d11ec7da48bd9
Simon Struck <simon@simonscode.org>
2021-04-06 08:07:55 +0200

    Remove files

--- src/TriangleChecker.java
+++ src/TriangleChecker.java
@@ -1,16 +1,6 @@
 public class TriangleChecker {
     public static TriangleType checkTriangle(int a, int b, int c) {
-        if (a == b && a == c) {
-            return TriangleType.EQUILATERAL_TRIANGLE;
-        } else if (a + b >= c && b + c >= a) {
-            if (a != b && b != c) {
-                return TriangleType.SCALENE_TRIANGLE;
-            } else {
-                return TriangleType.ISOSCELES_TRIANGLE;
-            }
-        } else {
-            return TriangleType.NO_TRIANGLE;
-        }
+        return TriangleType.NO_TRIANGLE;
     }
 }
 
